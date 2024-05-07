cb39d23549b66a46da50946cc7f07d634f5bbb5e
Abdallah-Salama2 <Abdallah.Salama.95@h-eng.helwan.edu.eg>
2024-05-07 18:41:42 +0300

    adding posts table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\PostFactory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -18,6 +20,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+         Post::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,44 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="">
+        <div class="float-end mt-10 mb-8 mr-2">
+            @role('')
+            <a href="#" class="m-2 p-4 bg-gray-700 rounded-3xl ">Create Post</a>
+        </div>
+        <div class="clear-end"></div>
+        <div class="">
+
+            <div class="py-2 align-middle inline-block min-w-full">
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50 dark:bg-gray-600 dark:text-gray-200">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Id</th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider">Title</th>
+                            <th scope="col" class="relative px-6 py-3">Edit</th>
+                        </tr>
+                        </thead>
+                        <tbody class="bg-white dark:bg-gray-700 divide-y divide-gray-200">
+                        <tr></tr>
+                        @foreach(\App\Models\Post::all() as $post)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">{{$post->id}}</td>
+                            <td class="px-6 py-4 whitespace-nowrap">{{$post->title}}</td>
+
+                            <td class="px-6 py-4 text-right text-sm">
+                                <a href="#" class="m-2 p-2 bg-gray-300 text-gray-700 rounded">Edit</a>
+                                <a href="#" class="m-2 p-2 bg-red-800 rounded">Delete</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                        <!-- More items... -->
+                        </tbody>
+                    </table>
+                    <div class="m-2 p-2">Pagination</div>
                 </div>
             </div>
         </div>
+
     </div>
 </x-app-layout>
